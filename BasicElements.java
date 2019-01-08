617b78c8e8b02e1a806fdeca1d4abb881278cfc2
Gnarleeey <Gnarley@yahoo.com>
2019-01-08 14:33:23 +0800

    JButton

--- BasicElements.java
+++ BasicElements.java
@@ -1,12 +1,15 @@
- import javax.swing.JFrame;
+ import javax.swing.*;
 
  	public class BasicElements{
  		public static void main(String[]args){
  			JFrame myFrame = new JFrame();
-
+ 			JButton myButton = new JButton();
  			myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
  			myFrame.setVisible(true);
  			myFrame.setSize(900,600);
+ 			myFrame.add(myButton);
+ 			myButton.setSize(100,100);
+ 			myFrame.setLayout(null);
 
  			System.out.println("Basic Java Elements");
 
